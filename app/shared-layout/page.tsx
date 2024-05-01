6a8ef56f3a1c092f9e26e5e981f3d68c03d4d55d
myoaung99 <myomyintaung064@gmail.com>
2024-05-01 11:17:51 +0630

    code update

--- app/shared-layout/page.tsx
+++ app/shared-layout/page.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import React, { useState } from "react";
-import { motion, Variants, AnimatePresence } from "framer-motion";
-import { ArrowBigLeft } from "lucide-react";
+import { motion, Variants, LayoutGroup, AnimatePresence } from "framer-motion";
+import { ArrowBigLeft, Trash } from "lucide-react";
 
 const notiList = [
   {
@@ -63,53 +61,84 @@ const notiItemVariants: Variants = {
     opacity: 1,
     y: 0,
   },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
 };
 
 const AnimatedNotificationList = () => {
   const [hoverItem, setHoverItem] = useState<number | null>(null);
+  const [itemList, setItemList] = useState(() => notiList);
 
   const handleOnHover = (itemId: number) => setHoverItem(itemId);
+  const handleRemove = (itemId: number) => {
+    const updatedNotiList = itemList.filter((noti) => noti.id !== itemId);
+    setItemList(updatedNotiList);
+  };
 
   return (
-    <AnimatePresence>
-      <div className="p-8 bg-gradient-to-r from-blue-400 to-cyan-300">
-        <motion.div
-          className="w-1/2"
-          initial="hidden"
-          animate="visible"
-          variants={notiListVariants}
-        >
-          <p className="mb-4 text-2xl antialiased font-semibold text-sky-800">
-            Animated Notification List
-          </p>
-          <motion.ul className="space-y-2">
-            {notiList.map((noti, i) => (
-              <li className="flex items-center gap-4">
-                <motion.div
-                  key={i}
-                  className="flex items-center gap-2 p-4 bg-white rounded shadow hover:cursor-pointer"
-                  whileHover={{
-                    scale: 1.05,
-                    boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-                    transition: { ease: "anticipate" },
-                  }}
-                  variants={notiItemVariants}
-                  onMouseEnter={() => handleOnHover(noti.id)}
-                  onMouseOver={() => handleOnHover(noti.id)}
-                >
-                  {i + 1}. <p className="text-teal-900">{noti.message}</p>
-                </motion.div>
-                {noti.id === hoverItem ? (
-                  <motion.div layoutId="arrow">
-                    <ArrowBigLeft size={44} fill="#fff" stroke="#fff" />
-                  </motion.div>
-                ) : null}
-              </li>
-            ))}
-          </motion.ul>
-        </motion.div>
-      </div>
-    </AnimatePresence>
+    <>
+      <AnimatePresence>
+        <div className="p-8 bg-gradient-to-r from-blue-400 to-cyan-300 min-h-svh">
+          <motion.div
+            className="w-1/2"
+            initial="hidden"
+            animate="visible"
+            variants={notiListVariants}
+          >
+            <p className="mb-4 text-2xl antialiased font-semibold text-sky-800">
+              အန်နီမိတ်တက် တန်ဖိုးရှိလှသော စကားများ
+            </p>
+            <motion.ul className="space-y-2">
+              <LayoutGroup>
+                {itemList.length > 0 ? (
+                  itemList.map((noti, i) => (
+                    <motion.li
+                      layout
+                      key={noti.id}
+                      exit={{
+                        opacity: 0,
+                      }}
+                      transition={{
+                        duration: 0.4,
+                      }}
+                      className="flex items-center gap-4 group"
+                    >
+                      <motion.div
+                        className="flex items-center justify-between p-4 bg-white rounded shadow hover:cursor-pointer"
+                        variants={notiItemVariants}
+                        onMouseEnter={() => handleOnHover(noti.id)}
+                        onMouseOver={() => handleOnHover(noti.id)}
+                      >
+                        <div className="flex items-center justify-between gap-10">
+                          <div className="flex items-center">
+                            {i + 1}.{" "}
+                            <p className="text-teal-900">{noti.message}</p>
+                          </div>
+                          <div onClick={() => handleRemove(noti.id)}>
+                            <Trash stroke="red" />
+                          </div>
+                        </div>
+                      </motion.div>
+                      {noti.id === hoverItem ? (
+                        <motion.div layoutId="arrow">
+                          <ArrowBigLeft size={44} fill="#fff" stroke="#fff" />
+                        </motion.div>
+                      ) : null}
+                    </motion.li>
+                  ))
+                ) : (
+                  <p>မရှိသေးပါ</p>
+                )}
+              </LayoutGroup>
+            </motion.ul>
+          </motion.div>
+        </div>
+      </AnimatePresence>
+    </>
   );
 };
 
